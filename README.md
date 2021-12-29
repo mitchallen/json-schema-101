ab6f187fe0132e507450a9ab6ec1c72867ef98b0
Mitch Allen <github@mitchallen.com>
2021-12-29 15:31:26 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to Validate JSON Input with NodeJS (Schema)
+
+This code is covered in this article:
+
+https://scriptable.com/blog/how-to-validate-json-input-with-nodejs-schema
---
+++ parser.js
@@ -0,0 +1,29 @@
+
+
+import { readFileSync } from 'fs';
+import Ajv from 'ajv';
+
+const ajv = new Ajv();
+
+export function parser( inputFile, schemaFile ) {
+
+    function readJsonFile(file) {
+        let raw = readFileSync(file);
+        return JSON.parse(raw);
+    }
+
+    let input = readJsonFile(inputFile);
+    let schema = readJsonFile(schemaFile);
+
+    const isValid = ajv.validate(schema, input);
+
+    if (!isValid) {
+        console.error(JSON.stringify(ajv.errors, null, 2));
+        return undefined;
+    }
+        
+    console.info('[INFO] Valid!');
+
+    return input;
+}
+
